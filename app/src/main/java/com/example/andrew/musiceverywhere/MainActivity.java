45d92ad116ffbc8d573244936dc85a13d96856bf
Pat <patrickkelley2015@gmail.com>
2018-10-13 15:42:45 -0400

    front end login screen

--- app/src/main/java/com/example/andrew/musiceverywhere/MainActivity.java
+++ app/src/main/java/com/example/andrew/musiceverywhere/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.andrew.musiceverywhere;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -9,7 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,33 +21,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final Context context = this.getApplicationContext();
-        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
         setContentView(R.layout.activity_main);
-        final Button button = findViewById(R.id.update);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                User firstUser = new User("Jeff");
-                Context context  = v.getContext();
+    }
 
-                ((TextView) findViewById(R.id.textbox)).setText("HEY");
-                if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    firstUser.updateData(context);
-                }
-                else{
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE},1);
-                }
-                if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    ((TextView) findViewById(R.id.textbox)).setText(firstUser.readData(context));
-                }
-                else{
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE},1);
-                    ((TextView) findViewById(R.id.textbox)).setText("NOOO");
-                }
-            }
-        });
+    public void spotifyLogin(View view) {
+        Intent intent = new Intent(this, UserInterface.class);
+        EditText editTextUser = (EditText) findViewById(R.id.text_username);
+        EditText editTextPass = (EditText) findViewById(R.id.text_password);
+        String userMessage = editTextUser.getText().toString();
+        String passMessage = editTextPass.getText().toString();
+        if(userMessage.equals("hack")&& passMessage.equals("umass")){
+            startActivity(intent);
+        }
+        startActivity(intent);
+
     }
 
 }
