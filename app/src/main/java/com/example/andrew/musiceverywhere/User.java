a8f4572771deddebf0ff78db2effa8d337e6385d
Andrew Cunningham <andrewcunnin@umass.edu>
2018-10-13 01:21:43 -0400

    add userinterface, start for file io

--- app/src/main/java/com/example/andrew/musiceverywhere/User.java
+++ app/src/main/java/com/example/andrew/musiceverywhere/User.java
@@ -1,10 +1,30 @@
 package com.example.andrew.musiceverywhere;
 
+import android.content.Context;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class User {
     private File userData;
     private String name;
-    private int lat, longi;
+    private int lati, longi;
     private String currentSong;
+
+    public User(){
+        userData = new File("user-data.txt");
+    }
+
+    public void updateData(){
+       /* try {
+            //OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("user-data.txt", Context.MODE_PRIVATE));
+            //outputStreamWriter.write(data);
+            //outputStreamWriter.close();
+        }
+        //catch(IOException e){
+          //  userData = new File("user-data.txt");
+        //}*/
+    }
 }
