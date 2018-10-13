7e7469dae198cf21cfad4cabdfb277dda0abfcd2
Andrew Cunningham <andrewcunnin@umass.edu>
2018-10-13 02:40:07 -0400

    add non-functioning io

--- app/src/main/java/com/example/andrew/musiceverywhere/MainActivity.java
+++ app/src/main/java/com/example/andrew/musiceverywhere/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.andrew.musiceverywhere;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_interface);
+        final Context context = this.getApplicationContext();
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.update);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                User firstUser = new User("Jeff");
+                Context context  = v.getContext();
+
+                ((TextView) findViewById(R.id.textbox)).setText("HEY");
+                if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    firstUser.updateData(context);
+                }
+                else{
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                }
+                if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    ((TextView) findViewById(R.id.textbox)).setText(firstUser.readData(context));
+                }
+                else{
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                    ((TextView) findViewById(R.id.textbox)).setText("NOOO");
+                }
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/andrew/musiceverywhere/User.java
+++ app/src/main/java/com/example/andrew/musiceverywhere/User.java
@@ -2,9 +2,11 @@ package com.example.andrew.musiceverywhere;
 
 import android.content.Context;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class User {
@@ -15,16 +17,42 @@ public class User {
 
     public User(){
         userData = new File("user-data.txt");
+        this.name = "";
+        this.currentSong = "";
     }
 
-    public void updateData(){
-       /* try {
-            //OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("user-data.txt", Context.MODE_PRIVATE));
-            //outputStreamWriter.write(data);
-            //outputStreamWriter.close();
+    public User(String name){
+        userData = new File("user-data.txt");
+        this.name = name;
+        this.currentSong = "";
+    }
+
+    public void updateData(Context context){
+       try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("user-data.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(name + "\n");
+            outputStreamWriter.write(currentSong + "\n");
+            outputStreamWriter.close();
         }
-        //catch(IOException e){
-          //  userData = new File("user-data.txt");
-        //}*/
+        catch(IOException e){
+           userData = new File("user-data.txt");
+        }
+    }
+
+    public String readData(Context context){
+        try {
+            InputStream is = context.getAssets().open("user-data.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line;
+            System.out.println("READING A USER");
+            if((line = reader.readLine()) != null){
+                return line;
+            }
+            return "No data";
+        }
+        catch(IOException e){
+            System.err.println("no data");
+            return "ERROR";
+        }
     }
 }
