57027dfa844c8a7802325ac87db2fd1286aee8b6
Andrew Cunningham <andrewcunnin@umass.edu>
2018-10-14 00:37:42 -0400

    show current tracks from users

--- app/src/main/java/com/example/andrew/musiceverywhere/Login.java
+++ app/src/main/java/com/example/andrew/musiceverywhere/Login.java
@@ -26,6 +26,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,10 +39,14 @@ import java.util.List;
 
 public class Login extends AppCompatActivity {
     private static LocationManager locationManager;
-    private static User currentUser = new User("Jeff");
+    private static User currentUser = new User("Somebody");
     private static DatabaseReference ref;
     private static final int FINE_LOCATION_PERMISSION = 1;
     private static ArrayList<User> users = new ArrayList<User>();
+    private static final String CLIENT_ID = "168b4446175b4585abf5f0366d0c727a";
+    private static final String REDIRECT_URI = "music-everywhere://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +62,7 @@ public class Login extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     FINE_LOCATION_PERMISSION);
-        }
-        else{
+        } else {
             setLocationmanager(); //sets location manager if granted
         }
         DBClient.writeToUser(currentUser);
@@ -74,13 +83,12 @@ public class Login extends AppCompatActivity {
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) { //bug where updating a user does not remove it
-                displayUsers(dataSnapshot.getValue(User.class));
+                displayUsers(dataSnapshot.getValue(User.class), false);
             }
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-                users.remove(dataSnapshot.getValue(User.class));
-                displayUsers();
+                displayUsers(dataSnapshot.getValue(User.class), true);
             }
 
             @Override
@@ -92,17 +100,6 @@ public class Login extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.d("QUERY", "CANCELLED");
             }
-        });
-
-
-        Button changeSong = (Button)findViewById(R.id.changeSong); //modifies song as proof of concept for live data updates
-        changeSong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentUser.setCurrentSong(currentUser.getCurrentSong()+"f");
-                DBClient.writeToUser(currentUser);
-            }
-
         });
     }
 
@@ -116,7 +113,7 @@ public class Login extends AppCompatActivity {
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     setLocationmanager();
                 } else {
-                  //disable location management
+                    //disable location management
                     //TODO
                 }
                 return;
@@ -124,25 +121,30 @@ public class Login extends AppCompatActivity {
         }
     }
 
-    public void displayUsers(){ //print all users in textview
+    public void displayUsers() { //print all users in textview
         ((TextView) findViewById(R.id.textView)).setText("");
-        for(User user: users) {
+        for (User user : users) {
             ((TextView) findViewById(R.id.textView)).append(user.getName() + " is listening to " + user.getCurrentSong() + "\n");
         }
     }
 
-    //if a user needs to be updated, pass in to update
-    public void displayUsers(User toUpdate){
+    //if a user needs to be updated, pass in to update or user to be removed
+    public void displayUsers(User toUpdate, Boolean remove) {
         ((TextView) findViewById(R.id.textView)).setText("");
         String id = toUpdate.getId();
         User curUser;
-        for(int i = 0; i < users.size(); i++) {
+        for (int i = 0; i < users.size(); i++) {
             curUser = users.get(i);
-            if(curUser.getId().equals(id)){
-                users.set(i, toUpdate);
-                curUser = users.get(i);
+            if (curUser.getId().equals(id)) {
+                if(remove){
+                    users.remove(i);
+                }
+                else {
+                    users.set(i, toUpdate);
+                    curUser = users.get(i);
+                }
             }
-            ((TextView) findViewById(R.id.textView)).append(curUser.getName() + " is listening to " + curUser.getCurrentSong() + "\n");
+            ((TextView) findViewById(R.id.textView)).append(curUser.getName() + " is listening to " + curUser.getCurrentSong() + "\n\n");
         }
     }
 
@@ -155,7 +157,7 @@ public class Login extends AppCompatActivity {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
                 currentUser.setLocation(location.getLongitude(), location.getLatitude()); //should write these changes instead of displaying users
-                displayUsers();
+                DBClient.writeToUser(currentUser);
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -174,6 +176,60 @@ public class Login extends AppCompatActivity {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1500, 0, locationListener);
     }
 
+    //Spotify Connection
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.CONNECTOR.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.CONNECTOR.disconnect(mSpotifyAppRemote);
+    }
+
+    private void connected() {
+        // Play a playlist
+        // mSpotifyAppRemote.getPlayerApi().play("spotify:user:spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState().setEventCallback(new Subscription.EventCallback<PlayerState>() {
+
+            public void onEvent(PlayerState playerState) {
+                final Track track = playerState.track;
+                if (track != null) {
+                    currentUser.setCurrentSong(track.name + " by " + track.artist.name + " from the album " + track.album.name);
+                    DBClient.writeToUser(currentUser);
+                }
+            }
+        });
+
+    }
 }
 
 
